fc8517b77a0c005852d1efbdad677de520a427bc
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-08-30 22:03:51 +0200

    Index and show actions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,12 @@
 class ContactsController < ApplicationController
   def index
+    @contacts = Contact.all
+    @prefix = "There are"
+    @size = @contacts.size
+  end
+
+  def show
+     @contact = Contact.find(params[:id])
+
   end
 end
