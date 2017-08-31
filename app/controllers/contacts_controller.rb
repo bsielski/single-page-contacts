eda20ae74b9dee7c81830c5c440229b6fe062898
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-08-31 18:11:01 +0200

    "create" and "destroy" actions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,4 +9,21 @@ class ContactsController < ApplicationController
      @contact = Contact.find(params[:id])
 
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
+
+  def destroy
+    Contact.find(params[:id])
+    @contact.destroy
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :phone_number)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :contacts, only: [:index, :show]
+  resources :contacts, only: [:index, :show, :create, :destroy]
   root "contacts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
