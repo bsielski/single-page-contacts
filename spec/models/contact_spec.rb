05840dda7d6770264e28aa8d008fde01cc3a459b
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-09-04 18:26:39 +0200

    It is not possible to create a contact with some fields missing

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:phone_number) }
+  
 end
