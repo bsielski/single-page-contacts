78fc927fa1b19280a423b0a069380e746020ac45
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-09-05 17:25:45 +0200

    Fix submit button text test

--- spec/views/contacts/index.html.erb_spec.rb
+++ spec/views/contacts/index.html.erb_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe "contacts/index.html.erb", type: :view do
   it "displays the submit button with proper name" do
     render
-    expect(rendered).to have_css("input[type=submit]", text: "Add contact")
+    expect(rendered).to have_css("input[type=submit][value='Add contact']")
   end
 end
