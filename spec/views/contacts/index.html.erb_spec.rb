0c65f92f60a2c79d29f33387d4e8c988e1a82158
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-09-05 20:58:28 +0200

    Add tests for search input and button

--- spec/views/contacts/index.html.erb_spec.rb
+++ spec/views/contacts/index.html.erb_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe "contacts/index.html.erb", type: :view do
     render
     expect(rendered).to have_css("input[type=submit][value='Add contact']")
   end
+
+  it "displays the search input" do
+    render
+    expect(rendered).to have_css("input[type=search]")
+  end
+
+  it "displays the search button with proper name" do
+    render
+    expect(rendered).to have_css("input[type=submit][value='Search']")
+  end
+
 end
