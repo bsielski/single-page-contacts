047ebb2674a5f5edccd517d49c015fd28ac121b8
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-09-06 22:23:18 +0200

    Add props and change div to form

--- app/assets/javascripts/components/contact_search.es6.jsx
+++ app/assets/javascripts/components/contact_search.es6.jsx
@@ -1,10 +1,10 @@
 class ContactSearch extends React.Component {
   render () {
     return (
-      <div className="contact_searcgh">
-        <input type="search" id="search_input"/>
+      <form onSubmit={this.props.handleSubmit} className="contact_searcgh">
+        <input type="search" id="search_input" onChange={this.props.handleChange} value={this.props.phrase}/>
         <input type="submit" value="Search"/>
-      </div>
+      </form>
     );
   }
 }
