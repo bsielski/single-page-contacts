7c2d5ca27a848816897dbec5eac555078c0b72e0
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-09-06 20:57:14 +0200

    Add id to search input

--- app/assets/javascripts/components/contact_search.es6.jsx
+++ app/assets/javascripts/components/contact_search.es6.jsx
@@ -2,7 +2,7 @@ class ContactSearch extends React.Component {
   render () {
     return (
       <div className="contact_searcgh">
-        <input type="search"/>
+        <input type="search" id="search_input"/>
         <input type="submit" value="Search"/>
       </div>
     );
