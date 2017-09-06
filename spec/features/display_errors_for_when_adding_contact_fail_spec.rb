590d368f70e6434c20e43be48ff32ed1883ffa4d
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-09-06 23:51:08 +0200

    More assertions less page reloads

--- spec/features/display_errors_for_when_adding_contact_fail_spec.rb
+++ spec/features/display_errors_for_when_adding_contact_fail_spec.rb
@@ -2,59 +2,76 @@ require 'rails_helper'
 
 RSpec.describe "Displaying errors", :type => :feature do
 
-  it "displays that first name can't be blank", js: true do
+  it "displays errors when no input", js: true do
     visit '/'
-    fill_in "First name", with: ""
     click_button "Add contact"
     expect(page).to have_content "First name can't be blank"
-  end
-
-  it "doesn't display that first name can't be blank", js: true do
-    visit '/'
-    fill_in "First name", with: "Something"
-    click_button "Add contact"
-    expect(page).to_not have_content "First name can't be blank"
-  end
-
-  it "displays that last name can't be blank", js: true do
-    visit '/'
-    fill_in "Last name", with: ""
-    click_button "Add contact"
     expect(page).to have_content "Last name can't be blank"
-  end
-
-  it "doesn't display that last name can't be blank", js: true do
-    visit '/'
-    fill_in "Last name", with: "Something"
-    click_button "Add contact"
-    expect(page).to_not have_content "Last name can't be blank"
-  end
-
-  it "displays that email can't be blank", js: true do
-    visit '/'
-    fill_in "Email", with: ""
-    click_button "Add contact"
     expect(page).to have_content "Email can't be blank"
-  end
-
-  it "doesn't display that email can't be blank", js: true do
-    visit '/'
-    fill_in "Email", with: "some@email.com"
-    click_button "Add contact"
-    expect(page).to_not have_content "Email can't be blank"
-  end
-
-  it "displays that phone number can't be blank", js: true do
-    visit '/'
-    fill_in "Phone number", with: ""
-    click_button "Add contact"
     expect(page).to have_content "Phone number can't be blank"
   end
 
-  it "doesn't display that phone number can't be blank", js: true do
+  it "displays errors when delete input", js: true do
     visit '/'
-    fill_in "Phone number", with: "555 333 222"
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    fill_in "Email", with: ""
+    fill_in "Phone number", with: ""
     click_button "Add contact"
+    expect(page).to have_content "First name can't be blank"
+    expect(page).to have_content "Last name can't be blank"
+    expect(page).to have_content "Email can't be blank"
+    expect(page).to have_content "Phone number can't be blank"
+  end
+
+  it "displays errors for first name", js: true do
+    visit '/'
+    fill_in "First name", with: ""
+    fill_in "Last name", with: "Something"
+    fill_in "Email", with: "some@mail.com"
+    fill_in "Phone number", with: "123 423 123"
+    click_button "Add contact"
+    expect(page).to have_content "First name can't be blank"
+    expect(page).to_not have_content "Last name can't be blank"
+    expect(page).to_not have_content "Email can't be blank"
+    expect(page).to_not have_content "Phone number can't be blank"
+  end
+
+  xit "displays errors for multiple tries", js: true do
+    visit '/'
+    fill_in "First name", with: ""
+    fill_in "Last name", with: "Something"
+    fill_in "Email", with: "some@mail.com"
+    fill_in "Phone number", with: "123 423 123"
+    click_button "Add contact"
+
+    expect(page).to have_content "First name can't be blank"
+    expect(page).to_not have_content "Last name can't be blank"
+    expect(page).to_not have_content "Email can't be blank"
+    expect(page).to_not have_content "Phone number can't be blank"
+
+    fill_in "First name", with: "SomeNotFakeName"
+    fill_in "Last name", with: ""
+    fill_in "Email", with: ""
+    fill_in "Phone number", with: "123 423 123"
+    click_button "Add contact"
+
+    expect(page).to have_content "Last name can't be blank"
+    expect(page).to have_content "Email can't be blank"
+    expect(page).to_not have_content "First name can't be blank"
+    expect(page).to_not have_content "Phone number can't be blank"
+  end
+
+  it "doesn't display errors when everything is OK", js: true do
+    visit '/'
+    fill_in "First name", with: "Somename"
+    fill_in "Last name", with: "Something"
+    fill_in "Email", with: "some@mail.com"
+    fill_in "Phone number", with: "123 423 123"
+    click_button "Add contact"
+    expect(page).to_not have_content "First name can't be blank"
+    expect(page).to_not have_content "Last name can't be blank"
+    expect(page).to_not have_content "Email can't be blank"
     expect(page).to_not have_content "Phone number can't be blank"
   end
 
